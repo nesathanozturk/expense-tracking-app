e5c0722f3cd306bd9446710546ad53b4d4deffbc
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-04 20:42:01 +0300

    feat: created data

---
+++ src/utils/data.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    name: "Converse Chuck 70",
+    price: 2000,
+    amount: 1,
+  },
+  {
+    id: 2,
+    name: "Trabzonspor Forması",
+    price: 600,
+    amount: 1,
+  },
+  {
+    id: 3,
+    name: "Nike Tişört",
+    price: 500,
+    amount: 1,
+  },
+  {
+    id: 4,
+    name: "iPhone 14 Pro Max",
+    price: 43000,
+    amount: 1,
+  },
+  {
+    id: 5,
+    name: "Nike Blazer Mid",
+    price: 2600,
+    amount: 1,
+  },
+  {
+    id: 6,
+    name: "Honda Civic",
+    price: 2.0,
+    amount: 1000000,
+  },
+];
